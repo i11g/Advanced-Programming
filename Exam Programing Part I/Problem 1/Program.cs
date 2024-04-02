33b0f97ff85d351dd2404286aa8a3f9114665623
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-02 07:13:08 +0300

    new files added

---
+++ Exam Programing Part I/Problem 1/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberOfStudents = int.Parse(Console.ReadLine());
+
+double average = 0;
+double sum = 0;
+for (int i = 0; i < numberOfStudents; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    sum += grade;
+}
+average = sum / numberOfStudents*1.00;
+Console.WriteLine($"{average:f2}");
