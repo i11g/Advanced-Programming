db787c44362fc487247635b0298100e1eecb06da
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-12 18:21:18 +0300

    files changed

---
+++ Exam Programing Part I/Problem 2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int rotations = int.Parse(Console.ReadLine());
+
+while(rotations > 0)
+{
+	for (int i = 0; i <num.Length-1; i++)
+	{
+		int temporaryElement = num[num.Length-1-i];
+		num[num.Length - 1-i] = num[num.Length - 1 - 1-i]; 
+        num[num.Length - 1- 1- i]= temporaryElement;
+	}
+	rotations--;
+}
+
+Console.WriteLine(string.Join(", ", num));
---
+++ Exam Programing Part I/Problem 4/Program.cs
@@ -0,0 +1,27 @@
+﻿static bool IsBalanced(string[] input)
+{
+    int balance = 0;
+
+    foreach (string symbol in input)
+    {
+        if (symbol == "(")
+        {
+            balance++;
+        }
+        else if (symbol == ")")
+        {
+            balance--;
+        }
+
+        if (balance < 0)
+        {
+            return false;
+        }
+    }
+
+    return balance == 0;
+}
+
+string[] input= Console.ReadLine().Split(' ').ToArray();
+bool result = IsBalanced(input);
+Console.WriteLine(result);
