b78d6ee96f86946c0053706cbc98a249ff611387
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-21 17:30:56 +0300

    files added

---
+++ Dictionaries/Count Real Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+static string Count(int[] nums)
+{
+    SortedDictionary<int, int> count = new();
+
+    foreach (int num in nums)
+    {
+        count.TryAdd(num, 0);
+        count[num]++;
+    }
+
+    StringBuilder sb = new();
+    foreach (KeyValuePair<int, int> pair in count)
+    {
+        sb.AppendLine($"{pair.Key} -> {pair.Value}");
+    }
+
+    return sb.ToString().Trim();
+}
+int[] input = { 1, 2, 3, 6, 8, 2, 3, }; 
+string result=Count(input);
+Console.WriteLine(result);
---
+++ Exam Preparation/CVS Parser/Program.cs
@@ -0,0 +1,14 @@
+﻿
+static string[] ParseCsv(string csvData)
+{
+    if (string.IsNullOrEmpty(csvData))
+    {
+        return Array.Empty<string>();
+    }
+
+    return csvData.Trim().Split(',', StringSplitOptions.TrimEntries);
+}
+
+string input = "sofia,lllg%,           pleben";
+string[] result= ParseCsv(input);
+Console.WriteLine(string.Join(",",result));
